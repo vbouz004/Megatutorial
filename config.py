1f915e3eb8a331f00ca47f0d44e5449b31a6a8bc
Victor Bouza Lavoy <vbouz004@fiu.edu>
2020-02-17 00:52:22 -0500

    Added Pagination for Posts

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
-
+    POSTS_PER_PAGE = 3
 
     """ Config for gmail
     export MAIL_SERVER=smtp.googlemail.com
