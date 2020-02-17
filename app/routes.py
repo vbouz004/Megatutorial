6b6bdd7fe19b2f423b84eaf9c9f89fb533c1f0af
Victor Bouza Lavoy <vbouz004@fiu.edu>
2020-02-17 00:41:28 -0500

    Added Explore Page
    Added Explore page to see all the posts and links to the posts' authors

--- app/routes.py
+++ app/routes.py
@@ -129,4 +129,8 @@ def unfollow(username):
     return redirect(url_for('user', username=username))
 
 
-
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
